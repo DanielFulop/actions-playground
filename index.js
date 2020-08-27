c35f6688cf8a774c721197cf6c1a8144a8075da1
Fulop Daniel <daniel.fulop@betssongroup.com>
2020-08-27 14:14:03 +0200

    get team

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    const githubToken = core.getInput('github-token');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload)
-    console.log(`The event payload: ${payload}`);
-    const octokit = github.getOctokit(githubToken);
-    console.log("github token:", githubToken)
-    console.log("Octokit instance:", octokit)
-    core.setFailed("Failing on purpose (mock)")
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        const githubToken = core.getInput('github-token');
+        console.log(`Hello ${nameToGreet}!`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        const octokit = github.getOctokit(githubToken);
+        const team = await octokit.teams.getByName('obg-fe-adaptive-release-gatekeepers');
+        console.log("Team:", team)
+        core.setFailed('Mocking a fail')
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
