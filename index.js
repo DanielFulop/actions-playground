448f02b2b3812ec817d3b3627c7ab94538e5a208
Fulop Daniel <daniel.fulop@betssongroup.com>
2020-08-27 11:04:30 +0200

    mocking fail

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    core.setFailed("Mocking a fail")
+    core.setFailed("Mocking a fail status")
 } catch (error) {
     core.setFailed(error.message);
 }
