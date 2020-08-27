4004a54697274feded24e144308544f38cf9b1c3
Fulop Daniel <daniel.fulop@betssongroup.com>
2020-08-27 11:44:12 +0200

    add octokit

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const github = require('@actions/github');
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
+    const githubToken = core.getInput('github-token');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context)
+    const payload = JSON.stringify(github.context.payload)
     console.log(`The event payload: ${payload}`);
-    core.setFailed(payload)
+    const octokit = github.getOctokit(githubToken);
+    console.log("github token:", githubToken)
+    console.log("Octokit instance:", octokit)
+    core.setFailed("Failing on purpose (mock)")
 } catch (error) {
     core.setFailed(error.message);
 }
